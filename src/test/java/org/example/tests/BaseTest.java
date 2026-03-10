e0e9e5ea0b67cf958936aa3071985ba984783bb5
simon5100 <simon51005100@gmail.com>
2026-03-10 23:04:58 +0700

    Initial commit

---
+++ src/test/java/org/example/tests/BaseTest.java
@@ -0,0 +1,9 @@
+package org.example.tests;
+
+public abstract class BaseTest {
+
+    // Константы для использования в API запросах
+    public static final String BASE_URL = "";
+    public static final String REGISTER_ENDPOINT = "";
+
+}
